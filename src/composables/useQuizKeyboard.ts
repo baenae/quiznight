1a6b3579065381e72ed5353dd3cd7a91d9e07a3b
besa <benjamin.sasse@engram.de>
2026-07-07 21:32:53 +0200

    feat: add keyboard composable for space/backspace navigation

---
+++ src/composables/useQuizKeyboard.ts
@@ -0,0 +1,21 @@
+import { onMounted, onUnmounted } from 'vue'
+
+export function useQuizKeyboard(onNext: () => void, onPrev: () => void) {
+  function handleKeydown(event: KeyboardEvent) {
+    if (event.key === ' ') {
+      event.preventDefault()
+      onNext()
+    } else if (event.key === 'Backspace') {
+      event.preventDefault()
+      onPrev()
+    }
+  }
+
+  onMounted(() => {
+    window.addEventListener('keydown', handleKeydown)
+  })
+
+  onUnmounted(() => {
+    window.removeEventListener('keydown', handleKeydown)
+  })
+}
